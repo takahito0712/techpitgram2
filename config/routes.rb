09f12da23f99d78ff62f2e9277d0b8d3f64197bf
takahito0712 <takahito1643@gmail.com>
2020-02-03 14:15:23 +0900

    ユーザー情報編集機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # ここにpagesコントローラーのhomeアクションのルーティングを追加する
-  get 'pages/home'
-  # この行を編集する
+  devise_for :users,
+  controllers: { registrations: 'registrations' } # ここの行を追加
   root 'pages#home'
+
+  # ここにusersコントローラーのshowアクションのルーティングを追加する
+  get '/users/:id', to: 'users#show', as: 'user'
 end
