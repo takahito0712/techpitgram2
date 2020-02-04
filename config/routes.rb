1b1cd1d93695a1874decde4e50498bec1198e5f1
takahito0712 <takahito1643@gmail.com>
2020-02-04 09:21:43 +0900

    一応完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users,
   controllers: { registrations: 'registrations' } # ここの行を追加
-  root 'pages#home'
+  root 'posts#index'
 
   # ここにusersコントローラーのshowアクションのルーティングを追加する
   get '/users/:id', to: 'users#show', as: 'user'
+
+  # get '/posts/new', to: 'posts#new'
+  # post '/posts', to: 'posts#create'
+  # post '/posts/:post_id/photos', to: 'photos#create', as: 'post_photos'  #下と同じ意味
+  resources :posts, only: %i(new create index show destroy) do
+    resources :photos, only: %i(create)
+end
 end
